461ab9d3b3f9ca3956b8178a14afde4e01b84449
Rachel Colby <rachelcolby11@gmail.com>
2016-04-14 17:32:27 -0700

    Add list of to-do's to Main.js controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,9 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.toDos = [
+      'Code',
+      'Groceries',
+      'Code some more'
     ];
   });
